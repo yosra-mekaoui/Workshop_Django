cca9c52b0463c171546cf845586e24c2954656ab
yosra-mekaoui <61566287+yosra-mekaoui@users.noreply.github.com>
2023-01-31 14:34:50 +0100

    firstCommit

---
+++ events/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from users.models import *
+# Create your models here.
+
+class Event(models.Model):
+    CATEGORY_CHOICES = (
+        ('Music', 'Music'),
+        ('Cinema', 'Cinema'),
+        ('Sport', 'Sport'),
+    )
+
+    title = models.CharField(max_length=255, null=True)
+    description = models.TextField()
+    eventImage = models.ImageField(upload_to='images/', blank=True)
+
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=8)
+    state = models.BooleanField(default=False)
+    nbrParticipants = models.IntegerField(default=0)
+    eventDate = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    organizer = models.ForeignKey(Person, on_delete=models.CASCADE)
