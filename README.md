76ccca6f6e95614972ff5f45bb2b5270cddae8b5
yosra-mekaoui <61566287+yosra-mekaoui@users.noreply.github.com>
2023-02-07 09:57:31 +0100

    commit_fields

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Workshop_Django4.1 
 
+This is a simple introduction to [Django](https://www.djangoproject.com/) projects.
+
+Things you'll need:
+
+- [Python](https://www.python.org/)
+- [Virtualenv](https://virtualenv.pypa.io/en/latest/)
+- [Django](https://www.djangoproject.com/)
+- [Pillow](https://python-pillow.org/)
+
+
 ## What you need to do
 
 1. > Install Python
--- events/admin.py
+++ events/admin.py
@@ -11,6 +11,64 @@ class EventAdmin(admin.ModelAdmin):
     )
     ordering = ('title',)
     list_filter = ('state', 'category')
+    # input de recherche
+    search_fields = [
+        'title',
+        'category'
+    ]
+    # pagination
+    list_per_page = 5
+
+
+
+    # bch nbadl l'ordre ta3 l'affichage de formulaire nhbouch yjini mnadhm kima ena hatitou fl code
+    # ('title','description') hedhom hatinhom fard ligne 5ater nhbhom fl formulaire ykounou bjnab b3dhhom
+
+    # bch nhotha fi commentaire 5ater ya nhotha hya ya nhot fieldsets
+
+    # fields = (
+    #     'organizer',
+    #     'state',
+    #     ('title','description'), 
+    #     'category',
+    #     'eventDate',
+    #     'eventImage',
+    #     'nbrParticipants',
+    #     ('created_at','updated_at')
+
+
+    # )
+
+
+
+
+    # hedhom juste kif nhb nra el created wl updated puisque houma njmch nbdl fihom donc n3ml readonly w nzidhom fl fields
+    readonly_fields =('created_at','updated_at')
+
+    # groupement des fileds
+    # 'classes' : ('collapse',),  hedhi zdnha kif ena nhb el fields hadhika ma todhrch direct ela ma ena n3ml show
+    fieldsets = (
+        (
+            'STATE',
+            {
+                'classes' : ('collapse',),
+                'fields': (('state','category'),
+                ('created_at','updated_at'))
+            }
+        ),
+        (
+            'ABOUT',
+            {
+                'fields': (
+                    'title',
+                    'description',
+                    'nbrParticipants',
+                    'eventImage',
+                    'eventDate'
+                )
+            }
+        )
+    )
 
 
 admin.site.register(Event, EventAdmin)
